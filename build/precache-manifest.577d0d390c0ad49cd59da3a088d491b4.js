c957e1ee28ca9ac5375ef6434d3f32c950ee8e08
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2021-03-11 15:33:38 +0530

    initial

---
+++ build/precache-manifest.577d0d390c0ad49cd59da3a088d491b4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ebce71f2dafecd3a2c4950f0c2b162",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a320c5feead0872f2183",
+    "url": "./static/css/2.4163fe8a.chunk.css"
+  },
+  {
+    "revision": "3a462c761b2306a06207",
+    "url": "./static/css/main.4c31039a.chunk.css"
+  },
+  {
+    "revision": "a320c5feead0872f2183",
+    "url": "./static/js/2.9096fba0.chunk.js"
+  },
+  {
+    "revision": "59f32a6c739ede0c4d3f15bb8fa93d53",
+    "url": "./static/js/2.9096fba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a462c761b2306a06207",
+    "url": "./static/js/main.ed99e4cf.chunk.js"
+  },
+  {
+    "revision": "51feb3a1c832632fcc80ab84bb3914ff",
+    "url": "./static/js/main.ed99e4cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfd358ef2f82b015afba",
+    "url": "./static/js/runtime-main.4d2a9d18.js"
+  },
+  {
+    "revision": "31d28485e1cf7369272270fd730327c0",
+    "url": "./static/media/remixicon.31d28485.eot"
+  },
+  {
+    "revision": "881fbc46361e0c0e5f003c159b2f3005",
+    "url": "./static/media/remixicon.881fbc46.woff"
+  },
+  {
+    "revision": "888e61f04316f10bddfff7bee10c6dd0",
+    "url": "./static/media/remixicon.888e61f0.ttf"
+  },
+  {
+    "revision": "95138f36e015ad912c37db92164f5844",
+    "url": "./static/media/remixicon.95138f36.svg"
+  },
+  {
+    "revision": "9915fef980fa539085da55b84dfde760",
+    "url": "./static/media/remixicon.9915fef9.woff2"
+  }
+]);
